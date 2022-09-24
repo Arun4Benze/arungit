5cc2702a2fd0520a3036701fa884639c570889de
Arun4Benze <arunbenz93@gmail.com>
2022-09-24 10:19:52 +0530

    commit

--- src/com/arun/git/Sample.java
+++ src/com/arun/git/Sample.java
@@ -3,6 +3,6 @@ package com.arun.git;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("master copy");
-		System.out.println("benz conflict trying copy");
+		System.out.println(" conflict trying copy");
 	}
 }
