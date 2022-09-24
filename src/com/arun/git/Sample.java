b77649601faf7dd435ce46878b1342e58d67263a
Arun4Benze <arunbenz93@gmail.com>
2022-09-24 09:43:03 +0530

    arrun commit next

--- src/com/arun/git/Sample.java
+++ src/com/arun/git/Sample.java
@@ -3,6 +3,6 @@ package com.arun.git;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("master copy");
-		System.out.println("arun copy");
+		System.out.println("arun copy new");
 	}
 }
